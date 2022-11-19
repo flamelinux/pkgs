dc07756badd27a55d9448503c13d6741c0d30b53
flamelinux <118629615+flamelinux@users.noreply.github.com>
2022-11-19 21:52:29 +0300

    Update flame-updater.py

--- global/flame-updater.py
+++ global/flame-updater.py
@@ -2,9 +2,9 @@
 import os
 import sys
 
-if sys.argv[1] == "install"
+if sys.argv[1] == "install":
  print("Done :D")
   
-if sys.argv[1] == "remove"
+if sys.argv[1] == "remove":
  os.system("rm flame-updater.py")
  print("Done :D")
